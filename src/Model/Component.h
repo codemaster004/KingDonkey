67247ed87c810a4a2b2d6b18e5aa2cc99ab907ef
Filip Dabkowski <filip.dabkowski@gmail.com>
2023-12-16 21:50:15 +0100

    finist component

---
+++ src/Model/Component.h
@@ -0,0 +1,41 @@
+//
+// Created by Filip Dabkowski on 16/12/2023.
+//
+
+#ifndef KINGDONKEY_COMPONENT_H
+#define KINGDONKEY_COMPONENT_H
+
+#include "cstddef"
+
+
+using ComponentID = size_t;
+
+inline ComponentID getComponentTypeId() {
+	static ComponentID lastId = 0;
+	return lastId++;
+}
+
+template<typename T>
+inline ComponentID getComponentTypeId() {
+	static ComponentID typeId = getComponentTypeId();
+	return typeId;
+}
+
+class Entity;
+
+class Component {
+private:
+
+public:
+	Entity *entity = nullptr;
+
+	virtual void init() {}
+
+	virtual void update() {}
+
+	virtual ~Component() = default;
+
+};
+
+
+#endif //KINGDONKEY_COMPONENT_H
