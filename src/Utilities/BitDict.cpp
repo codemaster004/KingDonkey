d1988f4653cad030f3af480a7aeb33415a4dbf74
Filip Dabkowski <filip.dabkowski@gmail.com>
2023-12-16 19:06:49 +0100

    bit dictionary structure

---
+++ src/Utilities/BitDict.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Filip Dabkowski on 16/12/2023.
+//
+
+#include "BitDict.h"
+
+
+uint64_t BitDict::mask(uint8_t index) {
+	return 1 << index;
+}
+
+bool BitDict::checkSize(int index) const {
+	return index > 0 && index < size;
+}
+
+void BitDict::set(int index) {
+	if (checkSize(index)) {
+		dict |= mask(index);
+	}
+}
+
+bool BitDict::get(int index) {
+	if (checkSize(index)) {
+		return dict >> index & 1;
+	}
+	return false;
+}
+
+void BitDict::remove(int index) {
+	if (checkSize(index)) {
+		dict &= ~mask(index);
+	}
+}
---
+++ src/Utilities/BitDict.h
@@ -0,0 +1,35 @@
+//
+// Created by Filip Dabkowski on 16/12/2023.
+//
+
+#ifndef KINGDONKEY_BITDICT_H
+#define KINGDONKEY_BITDICT_H
+
+#include "cstdint"
+
+
+class BitDict {
+private:
+	uint64_t dict;
+	uint8_t size;
+
+	static uint64_t mask(uint8_t index);
+
+	bool checkSize(int index) const;
+
+public:
+	explicit BitDict(int maxBits) : size((uint8_t) (maxBits)) {
+		dict = 0;
+	}
+
+	void set(int index);
+
+	bool get(int index);
+
+	void remove(int index);
+
+	~BitDict() = default;
+};
+
+
+#endif //KINGDONKEY_BITDICT_H
