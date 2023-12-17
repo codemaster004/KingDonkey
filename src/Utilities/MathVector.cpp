42695f3252004252aedfec7adb1dd79b80027e6d
Filip Dabkowski <filip.dabkowski@gmail.com>
2023-12-17 15:29:59 +0100

    create vector to hold location information

---
+++ src/Utilities/MathVector.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Filip Dabkowski on 17/12/2023.
+//
+
+#include "MathVector.h"
+
+
+bool MathVector::checkDims(const MathVector &vec1, const MathVector &vec2) {
+	return vec1.dimensions == vec2.dimensions;
+}
+
+MathVector &MathVector::add(const MathVector &vec) {
+	if (checkDims(*this, vec))
+		for (int i = 0; i < dimensions; ++i)
+			this->vector[i] -= vec.vector[i];
+
+	return *this;
+}
+
+MathVector &MathVector::subtract(const MathVector &vec) {
+	if (checkDims(*this, vec))
+		for (int i = 0; i < dimensions; ++i)
+			this->vector[i] -= vec.vector[i];
+
+	return *this;
+}
+
+MathVector &MathVector::multiply(const MathVector &vec) {
+	if (checkDims(*this, vec))
+		for (int i = 0; i < dimensions; ++i)
+			this->vector[i] *= vec.vector[i];
+
+	return *this;
+}
+
+MathVector &MathVector::divide(const MathVector &vec) {
+	if (checkDims(*this, vec))
+		for (int i = 0; i < dimensions; ++i)
+			this->vector[i] /= vec.vector[i];
+
+	return *this;
+}
---
+++ src/Utilities/MathVector.h
@@ -0,0 +1,71 @@
+//
+// Created by Filip Dabkowski on 17/12/2023.
+//
+
+#ifndef KINGDONKEY_MATHVECTOR_H
+#define KINGDONKEY_MATHVECTOR_H
+
+#include "cstddef"
+
+class MathVector {
+protected:
+	float *vector;
+	size_t dimensions;
+
+public:
+
+	explicit MathVector(size_t size) : dimensions(size) {
+		vector = new float[dimensions];
+		for (int i = 0; i < dimensions; ++i) {
+			vector[i] = 0;
+		}
+	}
+
+	static bool checkDims(const MathVector &vec1, const MathVector &vec2);
+
+	MathVector &add(const MathVector &vec);
+
+	MathVector &subtract(const MathVector &vec);
+
+	MathVector &multiply(const MathVector &vec);
+
+	MathVector &divide(const MathVector &vec);
+
+	// TODO: general getters & setters
+
+	~MathVector() {
+		delete[] vector;
+	}
+
+public:
+	MathVector() = default;
+};
+
+class Vector2D : public MathVector {
+public:
+	Vector2D() : MathVector(2) {}
+
+	Vector2D(float x, float y) : MathVector(2) {
+		vector[0] = x;
+		vector[1] = y;
+	}
+
+	float x() {
+		return vector[0];
+	}
+
+	void x(float value) {
+		vector[0] = value;
+	}
+
+	float y() {
+		return vector[1];
+	}
+
+	void y(float value) {
+		vector[1] = value;
+	}
+};
+
+
+#endif //KINGDONKEY_MATHVECTOR_H
