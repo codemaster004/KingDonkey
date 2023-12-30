4ea2c40cbfcbf493d2c8ebdaa6fd2be4c7f8b409
Filip Dabkowski <filip.dabkowski@gmail.com>
2023-12-30 21:51:16 +0100

    Add enhanced comments in Shape class

--- src/Utilities/Shapes/Shape.cpp
+++ src/Utilities/Shapes/Shape.cpp
@@ -2,66 +2,84 @@
 #include "../../ViewModel/CollisionViewModel.h"
 
 #include "cmath"
-#include "../DataStructures/ListSet.h"
 
-
-//Shape::Shape() {
-//	origin = Vector2D(0, 0);
-//}
 
 void Shape::addCorner(float x, float y) {
-	vertices.push(Vector2D((float) x, (float) y));
+	vertices.push(Vector2D((float) x, (float) y)); // Add new corner vector (x, y) to the shape.
 }
+
 
 size_t Shape::nEdges() {
-	size_t edges = vertices.getSize();
-	return edges <= 1 ? 0 : edges - 1;
+	// Getting the count of vertices in the shape.
+	size_t verticesCount = vertices.getSize();
+
+	// Return 0 if the shape does not have any points or is a single point.
+	return verticesCount <= 1 ? 0 : verticesCount - 1;
 }
+
 
 void Shape::initRect(float x, float y, float w, float h) {
-	updateOrigin(x, y);
-	addCorner(0, 0);
-	addCorner(0, h);
-	addCorner(w, h);
-	addCorner(w, 0);
+	// All corners are relative to the origin to optimise movement in a space.
+	setOrigin(x, y);
+
+	// Adding corners to the rectangle.
+	// The order matters for creating the shape edges.
+	addCorner(0, 0); // Top-left
+	addCorner(0, h); // Bottom-left
+	addCorner(w, h); // Bottom-right
+	addCorner(w, 0); // Top-right
 }
 
-void Shape::updateOrigin(float newX, float newY) {
+
+Vector2D *Shape::getOrigin() {
+	return &origin; // return the pointer to the origin vector
+}
+
+
+void Shape::setOrigin(float newX, float newY) {
+	// Set new values for the origin.
 	origin.x(newX);
 	origin.y(newY);
 }
 
-void Shape::calculateNormalAxes(ListSet<Vector2D> &buffer) {
-	size_t nVertices = vertices.getSize();
 
+void Shape::calculateNormalAxes(ListSet<Vector2D> &buffer) {
+	// Loop through each vertex.
+	size_t nVertices = vertices.getSize();
 	for (int i = 0; i < nVertices; i++) {
+		// Get the current vertex and the next one.
 		Vector2D vector1 = vertices.get(i);
 		Vector2D vector2 = vertices.get((i + 1) % nVertices);
 
-		Vector2D edgeVector = vector2 - vector1;
+		Vector2D edgeVector = vector2 - vector1; // Calculate the edge vector.
+
+		// Calculate the normal vector by rotating the edge vector 90 degrees counter-clockwise.
 		Vector2D normalVector(-edgeVector.y(), edgeVector.x());
 
+		// Normalize the obtained normal vector
 		float magnitude = sqrt(normalVector.x() * normalVector.x() + normalVector.y() * normalVector.y());
 		normalVector.scalarMultiply(1 / magnitude);
 
-		buffer.append(normalVector);
+		buffer.append(normalVector); // Add to the Set
 	}
 }
 
+
 void Shape::projectOntoAxis(Vector2D axis, ProjectionRange *shadow) {
+	// Initializing the min and max of the shadow with project of the first vertex onto the axis.
 	shadow->min = Vector2D::dotProduct(vertices.get(0) + origin, axis);
 	shadow->max = shadow->min;
 
+	// Now, iterate through the rest of the vertices.
 	for (int i = 1; i < nEdges(); i++) {
+		// Project the current vertex onto the axis.
 		float projection = Vector2D::dotProduct(vertices.get(i) + origin, axis);
 
+		// If this projection is less than the current min, update the min.
 		if (projection < shadow->min)
 			shadow->min = projection;
+		// If it is more than the current max, update the max.
 		if (projection > shadow->max)
 			shadow->max = projection;
 	}
-}
-
-Vector2D *Shape::getOrigin() {
-	return &origin;
 }
--- src/Utilities/Shapes/Shape.h
+++ src/Utilities/Shapes/Shape.h
@@ -9,7 +9,9 @@
 #include "../MathVector.h"
 #include "../DataStructures/ListSet.h"
 
+
 struct ProjectionRange;
+
 
 class Shape {
 private:
@@ -19,18 +21,74 @@ private:
 public:
 	Shape() = default;
 
+	/**
+	 * @brief Add a corner to the shape.
+	 *
+	 * @details This function adds a corner with the given x and y coordinates to the shape.
+	 *
+	 * @param x The x coordinate of the corner.
+	 * @param y The y coordinate of the corner.
+	 */
 	void addCorner(float x, float y);
 
+	/**
+	 * @brief Calculates the number of edges for the shape.
+	 *
+	 * @details This function returns the number of edges for the shape, based on the number of vertices.
+	 * If the shape has less than 2 vertices, it does not have any edges.
+	 *
+	 * @return The number of edges in the shape.
+	 */
 	size_t nEdges();
 
+	/**
+	 * @brief Initialize a rectangle shape with the given parameters.
+	 *
+	 * @details This function updates the origin of the shape and adds four corners
+	 * to create a rectangle shape.
+	 *
+	 * @param x The X coordinate of the origin.
+	 * @param y The Y coordinate of the origin.
+	 * @param w The width of the rectangle.
+	 * @param h The height of the rectangle.
+	 */
 	void initRect(float x, float y, float w, float h);
 
-	void updateOrigin(float newX, float newY);
+	/**
+	 * @brief Set new value for the origin vector.
+	 *
+	 * @param newX new value for the x coordinate.
+	 * @param newY new value for the Y coordinate.
+	 */
+	void setOrigin(float newX, float newY);
 
+	/**
+	 * @brief Returns a pointer to the origin of the shape.
+	 *
+	 * @return A pointer to the origin of the shape.
+	 */
 	Vector2D *getOrigin();
 
+	/**
+	 * @brief Calculates the normal axes of the shape and stores them in the given buffer.
+	 *
+	 * @details The normal axes are the perpendicular vectors to the edges of the shape.
+	 * For each edge, it calculates the edge vector and then determines the perpendicular normal vector.
+	 * The function then normalizes the normal vector and appends it to the buffer.
+	 *
+	 * @param buffer The list set to store the calculated normal axes.
+	 */
 	void calculateNormalAxes(ListSet<Vector2D> &buffer);
 
+	/**
+	 * @brief Project the shape onto a given axis and calculate the projection range.
+	 *
+	 * @details Projection is calculated by taking the dot product of each vertex of the shape with the specified axis.
+	 * The minimum and maximum values of the projection range are updated based on the calculated projections.
+	 *
+	 * @param axis The axis onto which the shape is projected.
+	 * @param shadow The projection range structure where the minimum and maximum values will be stored.
+	 */
 	void projectOntoAxis(Vector2D axis, ProjectionRange *shadow);
 };
 
