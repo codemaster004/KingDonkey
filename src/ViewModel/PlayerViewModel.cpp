60fdfa57d858375c3c2fce5a1c107978532e6118
Filip Dabkowski <filip.dabkowski@gmail.com>
2024-01-06 23:20:43 +0100

    Refactor input handling in PlayerViewModel
    The PlayerViewModel has been updated to simplify and clarify the logic controlling the player's input. Previously, the PlayerModel components were directly manipulated on key events. Now, these have been replaced with equivalent Keyboard component methods, which improves maintainability and readability. Moreover, commented out code and unnecessary variables have been removed.

--- src/ViewModel/PlayerViewModel.cpp
+++ src/ViewModel/PlayerViewModel.cpp
@@ -6,11 +6,17 @@
 #include "../Game.h"
 #include "../Model/Components/Keyboard.h"
 
+/**
+ * @brief Handles input events for the player.
+ *
+ * This function is responsible for processing SDL input events and updating the player's position
+ * and keyboard state accordingly.
+ *
+ * @param event The SDL_Event representing the input event.
+ * @param player The PlayerModel object.
+ */
 void PlayerViewModel::handleInput(SDL_Event event, PlayerModel *player) {
-	auto *position = player->getComponent<Position>();
-	auto *physics = player->getComponent<Physics>();
-	auto *animation = player->getComponent<Animation>();
-	auto *collision = player->getComponent<Collision>();
+	auto position = player->getComponent<Position>();
 	auto keyboard = player->getComponent<Keyboard>();
 
 	int key = event.key.keysym.sym;
@@ -18,18 +24,10 @@ void PlayerViewModel::handleInput(SDL_Event event, PlayerModel *player) {
 		case SDL_KEYDOWN:
 			if (key == SDLK_SPACE) {
 				keyboard->keyPressed(Keyboard::KeySpace);
-//				if (!physics->getGravity() && !collision->getCollision(Collision_Ladder)) {
-//					physics->setGravity(true);
-//					position->setSpeedY(Game::config.jumpSpeed);
-//				}
 			} else if (key == SDLK_UP) {
-				if (collision->getCollision(Collision_Ladder)) {
-					position->setSpeedY(-Game::config.walkingSpeed);
-				}
+				keyboard->keyPressed(Keyboard::ArrUp);
 			} else if (key == SDLK_DOWN) {
-				if (collision->getCollision(Collision_Ladder) && !collision->getCollision(Collision_LadderBottom)) {
-					position->setSpeedY(Game::config.walkingSpeed);
-				}
+				keyboard->keyPressed(Keyboard::ArrDown);
 			} else if (key == SDLK_RIGHT) {
 				keyboard->keyPressed(Keyboard::ArrRight);
 			} else if (key == SDLK_LEFT) {
@@ -43,6 +41,8 @@ void PlayerViewModel::handleInput(SDL_Event event, PlayerModel *player) {
 				keyboard->keyLifted(Keyboard::ArrRight);
 				keyboard->keyLifted(Keyboard::ArrLeft);
 			} else if (key == SDLK_DOWN || key == SDLK_UP) {
+				keyboard->keyLifted(Keyboard::ArrUp);
+				keyboard->keyLifted(Keyboard::ArrDown);
 				position->getSpeed()->setY(0);
 			}
 			break;
@@ -52,25 +52,52 @@ void PlayerViewModel::handleInput(SDL_Event event, PlayerModel *player) {
 }
 
 
+/**
+ * @brief Process the player's input to control their movement and actions.
+ *
+ * This function takes in a PlayerModel object and updates its Position, Collision, and Animation components based
+ * on the player's input from the Keyboard component. The function checks for specific key presses to determine
+ * the player's actions.
+ *
+ * @param player The PlayerModel object representing the player.
+ */
 void PlayerViewModel::processInput(PlayerModel *player) {
-	auto *physics = player->getComponent<Physics>();
 	auto *position = player->getComponent<Position>();
 	auto *collision = player->getComponent<Collision>();
 	auto *animation = player->getComponent<Animation>();
 
 	auto keyboard = player->getComponent<Keyboard>();
+
+	// If the Space key is pressed and the player is colliding with the ground but not a ladder,
+	// the player's vertical speed is set to the jump speed specified in the Game configuration.
 	if (keyboard->getKey(Keyboard::KeySpace)) {
 		if (collision->getCollision(Collision_Ground) && !collision->getCollision(Collision_Ladder)) {
-			position->setSpeedY(Game::config.jumpSpeed);
+			// For an edge case of glitching through a wall/floor from an angle when jumping
+			if (position->getSpeed()->getY() >= 0.01) {
+				position->setSpeedY(Game::config.jumpSpeed);
+			}
 		}
 	}
+
+	// If the right arrow key is pressed, the player's horizontal speed is set to the walking speed specified in the
+	// Game configuration, and the Animation component is set to the MovingRight state.
+	// the same is checked for left arrow key.
 	if (keyboard->getKey(Keyboard::ArrRight)) {
 		position->setSpeedX(Game::config.walkingSpeed);
 		animation->setAnimationState(MovingRight);
 	} else if (keyboard->getKey(Keyboard::ArrLeft)) {
 		position->setSpeedX(-Game::config.walkingSpeed);
 		animation->setAnimationState(MovingLeft);
-	} else {
+	} else { // If neither the right nor left arrow key is pressed, the player's horizontal speed is set to 0.
 		position->getSpeed()->setX(0);
+	}
+
+	// If the arrow key is pressed, the player's and player is on the ladder set the speed to walking speed.
+	// Down arrow key is handled similarly however it can not be pressed when player is at the bottom of a ladder.
+	if (keyboard->getKey(Keyboard::ArrUp) && collision->getCollision(Collision_Ladder)) {
+		position->setSpeedY(-Game::config.walkingSpeed);
+	} else if (keyboard->getKey(Keyboard::ArrDown) && collision->getCollision(Collision_Ladder) &&
+		!collision->getCollision(Collision_LadderBottom)) {
+		position->setSpeedY(Game::config.walkingSpeed);
 	}
 }
