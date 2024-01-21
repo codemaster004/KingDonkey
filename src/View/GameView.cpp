69fe0ca1762493a8952294c1ec7d99da746562a6
Filip Dabkowski <filip.dabkowski@gmail.com>
2024-01-21 17:04:07 +0100

    stupid info

--- src/View/GameView.cpp
+++ src/View/GameView.cpp
@@ -32,17 +32,17 @@ void GameView::render() const {
 	SDL_SetRenderDrawColor(Game::renderer, 111, 0, 0, 255); // Red for Collidable game elements
 	levelModel->objects.render();
 
-	SDL_Rect infoRect = {4, 4, SCREEN_WIDTH - 8, 36};
-
-	SDL_SetRenderDrawColor(Game::renderer, 50, 50, 50, 255);
-	SDL_RenderFillRect(Game::renderer, &infoRect);
-
-	float timePassed = timer->getTotalTime();
-	snprintf((char*) (text), 128, "Current fps value: %.1lf. In game time passed: %.1lf", currentFps, timePassed);
-	renderText((char*) (text), SCREEN_WIDTH / 2 - strlen(text) * 8 / 2, 10);
-
-	snprintf((char*) (text), 128, "Implemented requirements: 1, 2, 3, 4, A, H.");
-	renderText((char*) (text), SCREEN_WIDTH / 2 - strlen(text) * 8 / 2, 26);
+//	SDL_Rect infoRect = {4, 4, SCREEN_WIDTH - 8, 36};
+//
+//	SDL_SetRenderDrawColor(Game::renderer, 50, 50, 50, 255);
+//	SDL_RenderFillRect(Game::renderer, &infoRect);
+//
+//	float timePassed = timer->getTotalTime();
+//	snprintf((char*) (text), 128, "Current fps value: %.1lf. In game time passed: %.1lf", currentFps, timePassed);
+//	renderText((char*) (text), SCREEN_WIDTH / 2 - strlen(text) * 8 / 2, 10);
+//
+//	snprintf((char*) (text), 128, "Implemented requirements: 1, 2, 3, 4, A, H.");
+//	renderText((char*) (text), SCREEN_WIDTH / 2 - strlen(text) * 8 / 2, 26);
 }
 
 void GameView::renderText(char* string, int startX, int topY) const {
